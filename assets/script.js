00dfddb4e7dde1c1fd87009043310ac840002db7
chrisdhodges12 <102702357+chrisdhodges12@users.noreply.github.com>
2022-05-15 14:14:55 -0400

    finished functions-still needs bug fixes with highscore

--- assets/script.js
+++ assets/script.js
@@ -1,137 +1,3 @@
-//DOM elements 
-var startButton = document.getElementById ("start-btn");
-var containerEl = document.getElementById ("container");
-var questionEl = document.getElementById ("question");
-var answerButtons = document.getElementById ("answers");
-var timeEl = document.getElementById ("timer");
-var highScoreButton = document.getElementById ("highScore")
-var ShowHighScores = document.getElementById ("showHighScores");
-
-
-//Variables
-var timeRemaining = 5; 
-var questionNumber;
-var initials = "";
-var questionNUmber;
-var time;
-// var score = 0 ???????
-var finalScore;
-
-var highScore = JSON.parse(localStorage.getItem("highScores")) || [];
-
-
-startButton.addEventListener("click", startGame);
-// highScoreSection.addEventListener("click", displayHighScores);
-
-function startGame() {
-    startButton.classList.add("hide");
-    questionEl.classList.remove("hide");
-    answerButtons.classList.remove("hide");
-    questionNumber = [0];
-    ShowHighScores.innerHTML = "";
-//while...
-    startClock();
-    nextQuestion(questions[0]);
-}
-
-function nextQuestion(question) {
-    questionEl.innerText = question.question;
-    question.answers.forEach(answer => {
-        var button = document.createElement("button");
-        button.innerText = answer.text;
-        button.classList.add("button");
-        if (answer.correct) {
-          button.dataset.correct = answer.correct;
-        }
-        answerButtons.appendChild(button);
-        button.addEventListener("click", chooseAnswer);
-    });  
-}  
-
-function startClock() {
-    timeEl.innerHTML = ("Time Remaining: " + timeRemaining);
-    if (timeRemaining <= 0) {
-        endGame();
-    } 
-    else {
-        timeRemaining -= 1;
-        time = setTimeout(startClock, 1000);
-    }
-}
-
-function chooseAnswer(e) {
-    var answerSelection = e.target;
-  if (!answerSelection.dataset.correct) {
-    timeRemaining = timeRemaining - 10;
-    console.log(timer);
-  }
-  if (questionNumber == questions.length - 1) {
-    gameOver();
-  } else {
-    reset();
-    questionNumber++;
-    nextQuestion(questions[questionNumber]);
-    console.log(score);
-  }
-}
-    
-function reset() {
-    while (answerButtons.firstChild) {
-      answerButtons.removeChild(answerButtons.firstChild);
-    }
-    startButton.classList.remove("hide");
-}
-
-function endGame() {
-    clearInterval (time);
-    timeEl.innerHTML = "";
-    startButton.innerHTML = "Play again";
-    startButton.classList.remove("hide");
-    questionEl.classList.add("hide");
-    reset();
-    showScore();
-
-}
-  
-function showScore() {
-
-}
-
-function submitScore() {
-
-}
-
-function displayHighScores() {
-
-}
-
-
-
-
-
-
-//>>>>handle high score: copied from robot gladiators ----need to change 
-  //>>check local storage for high score
-//   var highScore = localStorage.getItem("high score");
-//   if(highScore +++ null) {
-//       highScore = 0;
-//   }
-//>>set high score 
-// if (playerInfo.money > highScore) {
-//     localStorage.setItem("high score", playerInfo.money);
-//     localStorage.setItem("name", playerInfo.name);
-
-
-//<<<<<<<<NEED FUNCTIONs 
-//>>>>>>show first question, determine true if correct, subtract time if wrong, move to next question
-//>> handle timer
-//>>>>>>subtract time from timer if wrong
-//>>End game when all questions are done or timer = 0
-//>>save score and initials (input form?)
-//>>display high scores 
-//
-
-
 var questions = [{
 
     question: "Which syntax is used when declaring a function?",
@@ -179,3 +45,145 @@ var questions = [{
     ]
 }
 ];
+
+
+
+//DOM elements 
+var startButton = document.getElementById ("start-btn");
+var containerEl = document.getElementById ("container");
+var questionEl = document.getElementById ("question");
+var answerButtons = document.getElementById ("answers");
+var timeEl = document.getElementById ("timer");
+var highScoreButton = document.getElementById ("highScore")
+var showHighScores = document.getElementById ("showHighScores");
+
+
+//Variables
+var timeRemaining = 75; 
+var questionNumber;
+var initials = "";
+var questionNUmber;
+var time;
+// var submitScores;
+// var score = 0 ???????
+var score = 0;
+
+var highScore = JSON.parse(localStorage.getItem("highScores"));
+
+
+startButton.addEventListener("click", startGame);
+showHighScores.addEventListener("click", displayScores);
+
+function startGame() {
+    startButton.classList.add("hide");
+    questionEl.classList.remove("hide");
+    answerButtons.classList.remove("hide");
+    questionNumber = [0];
+    showHighScores.innerHTML = "";
+    // while (answerButtons.firstChild) {
+    //     answerButtons.removeChild(answerButtons.firstChild);
+    //   }
+    startClock();
+    nextQuestion(questions[0]);
+}
+
+function nextQuestion(question) {
+    questionEl.innerText = question.question;
+    question.answers.forEach(answer => {
+        var button = document.createElement("button");
+        button.innerText = answer.text;
+        button.classList.add("button");
+        if (answer.correct) {
+          button.dataset.correct = answer.correct;
+        }
+        answerButtons.appendChild(button);
+        button.addEventListener("click", chooseAnswer);
+    });  
+}  
+
+function startClock() {
+    timeEl.innerHTML = ("Time Remaining: " + timeRemaining);
+    if (timeRemaining <= 0) {
+        endGame();
+    } 
+    else {
+        timeRemaining -= 1;
+        time = setTimeout(startClock, 1000);
+    }
+}
+
+function chooseAnswer(e) {
+    var answerSelection = e.target;
+  if (!answerSelection.dataset.correct) {
+    timeRemaining = timeRemaining - 10;
+  }
+  if (questionNumber == questions.length - 1) {
+    endGame();
+  } else {
+    reset();
+    questionNumber++;
+    nextQuestion(questions[questionNumber]);
+  }
+}
+    
+function reset() {
+    while (answerButtons.firstChild) {
+      answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+function endGame() {
+    clearInterval (time);
+    timeEl.innerHTML = "";
+    startButton.innerHTML = "Play again";
+    startButton.classList.remove("hide");
+    timeRemaining = 0; 
+    reset();
+    showScore();
+
+}
+  
+function showScore() {
+    var score = timeRemaining;
+    showHighScores.innerHTML = `Your score is ${timeRemaining}!<div id="init"> Name: <input type="text" name="initials" id="initials" placeholder="Enter Your Name"><button id="save-btn" class="save-btn btn" onclick="submitScores(event)" disabled>Save</button>`;
+    questionEl.classList.add("hide")
+    showHighScores.classList.remove("hide");
+    answerButtons.classList.add("hide");
+    var initials = document.getElementById("initials");
+    var saveButton = document.getElementById("save-btn");
+}
+
+function submitScore(e) {
+    var score = {
+    score: timeRemaining,
+    name: username.value
+    };
+    highScore.push(score);
+    highScore.sort((a, b) => b.score - a.score);
+    
+    localStorage.setItem("highScores", JSON.stringify(highScore));
+    displayScores();
+}
+
+
+function displayScores() {
+    clearInterval(time);
+    // timEl.innerHTML = "";
+    reset();
+    questionEl.innerText = "";
+    showHighScores.innerHTML = `<h2>High Scores</h2><ul id="highScoresList"></ul>`
+    var highScoresList = document.getElementById("highScoresList");
+    highScoresList.innerHTML = highScore.map(score => {
+        return `<li class="scoresList">${score.name} - ${score.score}</li>`;
+    })
+    .join("");
+}
+
+    startButton.classList.remove("hide");
+    showHighScores.classList.add("hide");
+    
+
+
+
+
+
