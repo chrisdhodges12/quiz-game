f880089baa5304ffd37b032e3e1aabeaf295dd17
chrisdhodges12 <102702357+chrisdhodges12@users.noreply.github.com>
2022-05-15 14:54:08 -0400

    add readme, gitignore, and cleanup code

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# quiz-game
+# JavaScript Quiz Game
+
+## Purpose
+This application is a game that allows the user to test their knowledge of JavaScript. 
+
+Once the START button is clicked a question is presented.  Once an answer is selected a new question appears.  The timer begins at 75 seconds and for each wrong answer 10 seconds are subtracted.  Once the timer reaches 0 or all the questions are answered, the final score is equal to the time remaining.  
+
+High Scores are recorded into local storage. 
+
+
+## Built with
+*HTML
+*CSS
+*JavaScript
+
+## Website Link 
+
+https://chrisdhodges12.github.io/quiz-game/
+
+
+<img src="assets/images/screenshot1.jpg" width="600px" height="500px">
+
+<img src="assets/images/screenshot2.jpg" width="600px" height="500px">
+
+<img src="assets/images/screenshot3.jpg" width="600px" height="500px">
+
+
+
+## Developed by Chris Hodges 
--- assets/script.js
+++ assets/script.js
@@ -46,8 +46,6 @@ var questions = [{
 }
 ];
 
-
-
 //DOM elements 
 var startButton = document.getElementById ("start-btn");
 var containerEl = document.getElementById ("container");
@@ -57,36 +55,36 @@ var timeEl = document.getElementById ("timer");
 var highScoreButton = document.getElementById ("highScore")
 var showHighScores = document.getElementById ("showHighScores");
 
-
 //Variables
 var timeRemaining = 75; 
 var questionNumber;
 var initials = "";
+var score = 0;
 var questionNUmber;
 var time;
-// var submitScores;
-// var score = 0 ???????
-var score = 0;
 
+//local storage for high scores 
 var highScore = JSON.parse(localStorage.getItem("highScores"));
 
-
+//event listeners 
 startButton.addEventListener("click", startGame);
 showHighScores.addEventListener("click", displayScores);
 
+//function to begin game
 function startGame() {
     startButton.classList.add("hide");
     questionEl.classList.remove("hide");
     answerButtons.classList.remove("hide");
     questionNumber = [0];
     showHighScores.innerHTML = "";
-    // while (answerButtons.firstChild) {
-    //     answerButtons.removeChild(answerButtons.firstChild);
-    //   }
+    while (answerButtons.firstChild) {
+        answerButtons.removeChild(answerButtons.firstChild);
+      }
     startClock();
     nextQuestion(questions[0]);
 }
 
+//calls next question after answering
 function nextQuestion(question) {
     questionEl.innerText = question.question;
     question.answers.forEach(answer => {
@@ -101,6 +99,7 @@ function nextQuestion(question) {
     });  
 }  
 
+//runs clock when game begins 
 function startClock() {
     timeEl.innerHTML = ("Time Remaining: " + timeRemaining);
     if (timeRemaining <= 0) {
@@ -112,6 +111,7 @@ function startClock() {
     }
 }
 
+//handles answer selections, removes time if incorrect 
 function chooseAnswer(e) {
     var answerSelection = e.target;
   if (!answerSelection.dataset.correct) {
@@ -125,24 +125,26 @@ function chooseAnswer(e) {
     nextQuestion(questions[questionNumber]);
   }
 }
-    
+
+//clears out unwanted answer buttons 
 function reset() {
     while (answerButtons.firstChild) {
       answerButtons.removeChild(answerButtons.firstChild);
     }
 }
 
+//ends game, takes you to scores
 function endGame() {
     clearInterval (time);
     timeEl.innerHTML = "";
     startButton.innerHTML = "Play again";
-    startButton.classList.remove("hide");
-    timeRemaining = 0; 
+    startButton.classList.remove("hide"); 
     reset();
     showScore();
 
 }
-  
+ 
+//shows your score = time remaining 
 function showScore() {
     var score = timeRemaining;
     showHighScores.innerHTML = `Your score is ${timeRemaining}!<div id="init"> Name: <input type="text" name="initials" id="initials" placeholder="Enter Your Name"><button id="save-btn" class="save-btn btn" onclick="submitScores(event)" disabled>Save</button>`;
@@ -153,6 +155,7 @@ function showScore() {
     var saveButton = document.getElementById("save-btn");
 }
 
+// handles score submit into local storage
 function submitScore(e) {
     var score = {
     score: timeRemaining,
@@ -165,10 +168,9 @@ function submitScore(e) {
     displayScores();
 }
 
-
+//display score when called upon and at end of game 
 function displayScores() {
     clearInterval(time);
-    // timEl.innerHTML = "";
     reset();
     questionEl.innerText = "";
     showHighScores.innerHTML = `<h2>High Scores</h2><ul id="highScoresList"></ul>`
@@ -179,8 +181,8 @@ function displayScores() {
     .join("");
 }
 
-    startButton.classList.remove("hide");
-    showHighScores.classList.add("hide");
+startButton.classList.remove("hide");
+showHighScores.classList.add("hide");
     
 
 
